28cc0f444d3f8631dbfc1ce785c0b84a1d25f7b1
Barnabas Gonda <barnabas.gonda@googlemail.com>
2024-03-06 22:05:37 +0100

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  email:string = "oooooooooooooooooo";
+  password:string = "";
+
+  
+
+login() {
+
+}
+
+}
